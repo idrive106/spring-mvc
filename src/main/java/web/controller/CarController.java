4430d6a5d83bfca8404c043796fb9cceab8f4081
Vadim Levtcov <idrive106@gmail.com>
2025-03-26 15:35:56 +0400

    bugs fixed

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,12 +1,12 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.service.CarService;
-import web.service.CarServiceImp;
 
 
 @Controller
@@ -14,8 +14,9 @@ public class CarController {
 
     private final CarService carService;
 
-    public CarController() {
-        this.carService = new CarServiceImp();
+    @Autowired
+    public CarController(CarService carService) {
+        this.carService = carService;
     }
 
     @GetMapping("/cars")
--- src/main/java/web/service/CarServiceImp.java
+++ src/main/java/web/service/CarServiceImp.java
@@ -1,22 +1,20 @@
 package web.service;
 
+import org.springframework.stereotype.Service;
 import web.model.Car;
-
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+@Service
 public class CarServiceImp implements CarService {
 
-    private final List<Car> cars;
-
-    public CarServiceImp() {
-        cars = new ArrayList<>();
-        cars.add(new Car("BMW", "M4", "Синий", 575, "/image/M4Blue.webp"));
-        cars.add(new Car("BMW", "M8", "Серый", 575, "/image/M8Grey.webp"));
-        cars.add(new Car("BMW", "X5", "Белый", 555, "/image/X5White.webp"));
-        cars.add(new Car("BMW", "X6", "Голубой", 555, "/image/X6Blue.webp"));
-        cars.add(new Car("BMW", "Z4", "Красный", 425, "/image/Z4Red.webp"));
-    }
+    private final List<Car> cars = Arrays.asList(
+            new Car("BMW", "M4", "Синий", 575, "/image/M4Blue.webp"),
+            new Car("BMW", "M8", "Серый", 575, "/image/M8Grey.webp"),
+            new Car("BMW", "X5", "Белый", 555, "/image/X5White.webp"),
+            new Car("BMW", "X6", "Голубой", 555, "/image/X6Blue.webp"),
+            new Car("BMW", "Z4", "Красный", 425, "/image/Z4Red.webp")
+    );
 
     @Override
     public List<Car> getCars(Integer count) {
@@ -25,9 +23,6 @@ public class CarServiceImp implements CarService {
         }
         return cars.subList(0, count);
     }
-
-    public List<Car> getAllCars() {
-        return cars;
-    }
 }
+
 
